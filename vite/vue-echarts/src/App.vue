234d5650521d01a030fe7b8d96b370325b7e98c8
Xuqian Zhang <zxuqian@gmail.com>
2021-03-26 22:03:55 +0800

    Add vue-echarts

---
+++ vite/vue-echarts/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrapper">
+    <BarChart class="chart" />
+  </div>
+</template>
+
+<script setup>
+import BarChart from "./components/BarChart.vue";
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  background: hsl(210deg, 20%, 10%);
+}
+.wrapper {
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  place-items: center;
+}
+.wrapper .chart {
+  width: 50%;
+  height: 70%;
+}
+</style>
