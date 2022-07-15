85f1d1adf5bc37616b7c7d103b383821d43d25e6
Xuqian Zhang <zxuqian@gmail.com>
2022-07-15 12:01:25 +0800

    postcss preset env

--- postcss/postcss.config.js
+++ postcss/postcss.config.js
@@ -1,3 +1,9 @@
+const postcssPresetEnv = require("postcss-preset-env");
 module.exports = {
-  plugins: [require("autoprefixer")],
+  plugins: [
+    require("autoprefixer"),
+    postcssPresetEnv({
+      stage: 0,
+    }),
+  ],
 };
