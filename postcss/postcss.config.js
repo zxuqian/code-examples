4ecc90ccd403f53c96585e4f4fcea31ab44883d6
Xuqian Zhang <zxuqian@gmail.com>
2022-07-15 12:09:13 +0800

    Add stylelint

--- postcss/postcss.config.js
+++ postcss/postcss.config.js
@@ -1,6 +1,7 @@
 const postcssPresetEnv = require("postcss-preset-env");
 module.exports = {
   plugins: [
+    require("stylelint"),
     require("autoprefixer"),
     postcssPresetEnv({
       stage: 0,
