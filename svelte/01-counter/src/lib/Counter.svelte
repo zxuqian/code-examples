699b4ca654a02fdc220da4c0370d77d8222cf9ea
Xuqian Zhang <zxuqian@gmail.com>
2023-12-30 18:03:03 +0800

    add svelte example

---
+++ svelte/01-counter/src/lib/Counter.svelte
@@ -0,0 +1,13 @@
+<script>
+  import ShowCount from "./ShowCount.svelte";
+
+  let count = 0;
+  const increment = () => {
+    count += 1;
+  };
+</script>
+
+<button on:click={increment}>
+  count is {count}
+</button>
+<ShowCount {count} />
---
+++ svelte/01-counter/src/lib/ShowCount.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let count = 0;
+
+  let arr = [1, 2, 3, 4, 5];
+</script>
+
+{#each arr as a}
+  <p>
+    {a}
+  </p>
+{/each}
+
+{#if count > 5}
+  计数大于了 5
+{/if}
